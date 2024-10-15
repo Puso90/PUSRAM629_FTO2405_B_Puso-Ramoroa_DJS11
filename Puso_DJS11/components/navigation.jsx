dc487f4cc23f65b0bd7dbf93eb1959f2eb8d2ef4
Puso90 <juwishbrands@gmail.com>
2024-10-15 10:04:02 +0200

    implementing the header

---
+++ Puso_DJS11/components/navigation.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+
+export default function Header() {
+    
+
+    
+
+    return (
+        <header>
+            
+            <nav className="nav-bar">
+                <input type="search"></input>
+                <h3>Puso's Podcast Machine</h3>
+               
+            </nav>
+        </header>
+    )
+}
--- Puso_DJS11/src/App.jsx
+++ Puso_DJS11/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Podcasts from '../components/Podcast_page';
 import Footer from "../components/footer";
+import Navigation from "../components/navigation";
 
 function App() {
   
 
   return (
     <div>
+      <Navigation />
       <Podcasts />
       <Footer />
     </div>
