76cb68d48703006e9d037bf4d001d797b6649c7f
Puso90 <juwishbrands@gmail.com>
2024-10-15 08:34:27 +0200

    created components and renaming correctly

--- Puso_DJS11/src/App.jsx
+++ Puso_DJS11/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import BlogPosts from '../components/BlogPosts';
+import Podcasts from '../components/Podcast_page';
 
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div>
-      <BlogPosts />
+      <Podcasts />
     </div>
   )
 }
