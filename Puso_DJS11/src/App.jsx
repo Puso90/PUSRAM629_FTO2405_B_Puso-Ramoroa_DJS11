1a3ac906736ade69bd8c68aff026401d8906e39f
Puso90 <juwishbrands@gmail.com>
2024-10-16 17:57:20 +0200

    Implementing routing at App.jsx;  install react-router-dom

--- Puso_DJS11/src/App.jsx
+++ Puso_DJS11/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   
 
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
