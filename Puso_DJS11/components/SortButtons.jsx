d5db636747403e88259b580eb88358964605429b
Puso90 <juwishbrands@gmail.com>
2024-10-18 00:09:10 +0200

    adding sort buttons

--- Puso_DJS11/components/SortButtons.jsx
+++ Puso_DJS11/components/SortButtons.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import "./Style/SortButtons.css"; // Adjust path if necessary
+import "./Style/SortButtons.css";
+import {FaHeart} from "react-icons/fa"
 
-export default function SortButtons() {
+export default function SortButtons({ onSort }) {
     return (
         <div className="SortButtons"> 
-            <button className='sortButton allPodcasts'>
+            <button className='sortButton allPodcasts' onClick={() => onSort('All')}>
                 All Podcasts
             </button>
 
-            <button className='sortButton aToZ'>
+            <button className='sortButton aToZ' onClick={() => onSort('A-Z')}>
                 Sort: A-Z
             </button>
 
-            <button className='sortButton zToA'>
+            <button className='sortButton zToA' onClick={() => onSort('Z-A')}>
                 Sort: Z-A
             </button>
 
             <button className='sortButton genre dropDownButton'>
                 Sort by genre
                 {/* Optionally add dropdown here */}
+            </button>
+
+            <button className='sortButton favourites'>
+                <FaHeart />
             </button>
         </div>
     )
