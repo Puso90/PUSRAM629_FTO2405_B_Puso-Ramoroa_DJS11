0e80b64544e086ef1e9f58aba10cadac53185d3f
Puso90 <juwishbrands@gmail.com>
2024-10-18 00:06:52 +0200

    added link to homepage from podcast detail page

--- Puso_DJS11/components/header.jsx
+++ Puso_DJS11/components/header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import SearchBar from "/components/Search/searchbar.jsx"; 
 import { SearchResultsList } from "./Search/SearchResultsList";
 import "./Style/header.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
     
@@ -14,14 +15,15 @@ export default function Header() {
             <nav className="nav-bar">
 
             <div className="div-app-name">
+                <Link to={'/'}>
                     <h3>Puso's Podcast Machine</h3>
+                </Link>    
                 </div>
                 
                 <div className="search-bar-container">
                     <SearchBar setResults={setResults}/>
                     <SearchResultsList results={results}/>
                 </div>
-
                 
                 
                
