bb491077475c8b833b57ec9f440aba0398dbf727
Puso90 <juwishbrands@gmail.com>
2024-10-18 18:29:07 +0200

    create favourites route page

--- Puso_DJS11/src/App.jsx
+++ Puso_DJS11/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/podcast/:id" element={<PodcastDetail />} />
+        <Route path="/favourites" element={<Favourites />} />
       </Routes>
     </Router>
   )
