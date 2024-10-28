380745db5c8509740d75a12dace31da5276f03b2
Puso90 <juwishbrands@gmail.com>
2024-10-28 13:24:43 +0200

    favourite page refactoring

---
+++ src/pages/FavouritesPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Favourites from "../components/Favourites_page";
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+export default function FavouritesPage() {
+
+    return (
+        <div>
+            <Header />
+            <Favourites />
+            <Footer />
+        </div>
+    )
+}
